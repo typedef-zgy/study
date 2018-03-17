a74eefbf716b0f58e2ed8dfe1ede09e2da21a288
zhanggy <zhanggy@formail.com>
2018-03-18 02:13:34 +0800

    template note and some vim note

--- cpp/template_test/class_template/class_temp.cpp
+++ cpp/template_test/class_template/class_temp.cpp
@@ -1,31 +1,82 @@
-//purpose of the test:
+#include <iostream>
+using std::cout;
+using std::endl;
+
 //1.test class name and type name inner class.Yeah,now i know,this is called injected class name
 template<typename T>
-class Test
+class TestInjected
 {
 public:
-	typedef Test this_type_i;
-	typedef Test<T> this_type_o;
-	Test(){}
-	Test(Test const & r){ typename T::value_type value; }
-	template<typename Y> Test(Test<Y> const &){}
-	~Test(){}
+	TestInjected(){}
+	TestInjected(TestInjected const & r){ }
+	template<typename Y> TestInjected(TestInjected<Y> const &){}
+	~TestInjected(){}
 
-	Test &  operator=(Test const & r){}
+	//TestInjected is ok.TestInjected<T> is ok.
+	TestInjected &  operator=(TestInjected const & r){}
+};
+
+//2.test specialization
+template<typename T1, typename T2>
+class TestSpec
+{
+public:
+	TestSpec(){ cout << "base template TestSpec" << endl; }
 };
 
 template<typename T>
-struct type_trails_test
+class TestSpec<T, T>
 {
-	typedef T value_type;
+public:
+	TestSpec() { cout << "partial template TestSpec" << endl; }
 };
+
+template< >
+class TestSpec<void, void>
+{
+public:
+	TestSpec() { cout << "full specialization template TestSpec" << endl; }
+};
+
+//3.non-type parameters
+//see cppreference "Template non-type arguments"
+template<typename T, int MAX_SIZE>
+class TestNonType
+{
+public:
+	TestNonType() { cout << "[Non-Type]MAX_SIZE:" << MAX_SIZE << endl; }
+};
+
+template<const char* name>
+class TestNonTypeLink
+{
+public:
+	TestNonTypeLink() { cout << "[Non-Type]TestNonTypeLink name:" << name << endl; }
+};
+
+const char* internal = "internal";
+extern const char* external = "external";
+const char internal1[] = "internal1";
+extern const char external1[] = "external1";
 
 int main()
 {
-	Test< type_trails_test<int> > t;
-	Test< type_trails_test<int> > t1(t);
-	Test< type_trails_test<int> >::this_type_i t2;
-	Test< type_trails_test<int> >::this_type_o t3;
+	//test2
+	//partial ordering(cppreference)
+	TestSpec<int, int> spec1;
+	//can't compiled.Only is the primary template is found by name
+	//its partial specializations are considered.
+	//TestSpec<int> spec2;
+	TestSpec<void, void> spec3;
+
+	//test3
+	TestNonType<int, 5> non_type1;
+	TestNonType<int, 10> non_type2;
 	
+	//TestNonTypeLink<internal> non_type_inter; //err:internal and external is no a adderss of an variable.
+	//TestNonTypeLink<external> non_type_exter;
+	//TestNonTypeLink<internal1> non_type_inter1;
+	TestNonTypeLink<external1> non_type_exter1;
+
 	return 0;
 }
